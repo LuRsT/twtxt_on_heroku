d927f91b8b9af954ab4df04ac877e87db067f738
Gil Gonçalves <lursty@gmail.com>
2019-08-25 19:48:56 +0100

    Fix typo in dependency python-dateutil

--- app.py
+++ app.py
@@ -24,13 +24,13 @@ HEADER = """
 def twtxt():
     response = requests.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vT5pcDrp03CkitXKjeTZ8PCwSXHbmEmtvQVidB6XdbjhgmIc8Y6snNZK5XZU2-VhapSXWPZwsSYNf6q/pub?output=csv")
 
-    txtxts = response.text.replace(",", "\t")
+    twtxts = response.text.replace(",", "\t")
     formatted_lines = []
-    for line in twtxts:
-        date, content = line.split("\t")[0], line.split(',')[:1]
+    for line in twtxts.split("\n"):
+        date, content = line.split("\t")
         dt = parser.parse(date)
-        date = dt.astimezone(pytz.timezone('Europe/London'))
-        formatted_lines.append("\t".join([date, content]))
+        formatted_date = dt.astimezone(pytz.timezone('Europe/London'))
+        formatted_lines.append("\t".join([str(formatted_date), content]))
 
     body = HEADER + "\n".join(formatted_lines)
     return Response(body, mimetype='text/plain')
