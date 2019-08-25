af855f3abbe6dd71603644c3ce9b80e471fe6998
Gil Gonçalves <lursty@gmail.com>
2019-08-25 19:10:02 +0100

    Add HEADER + url for my google sheet
    NOTE: Later they will be variables

--- app.py
+++ app.py
@@ -4,7 +4,23 @@ import requests
 
 app = Flask(__name__)
 
+HEADER = """
+#
+# twtxt is an open, distributed microblogging platform
+#
+# == Metadata ==
+#
+# nick = gil
+# url = https://twtxt.herokuapp.com/
+# user_agent = twtxt.xyz (+http://twtxt.xyz)
+#
+# == Content ==
+#
+"""
 
 @app.route('/')
 def twtxt():
-    return 'Hello, World!'
+    response = requests.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vT5pcDrp03CkitXKjeTZ8PCwSXHbmEmtvQVidB6XdbjhgmIc8Y6snNZK5XZU2-VhapSXWPZwsSYNf6q/pub?output=csv")
+
+    body = HEADER + response.text.replace(",", "\t")
+    return body
